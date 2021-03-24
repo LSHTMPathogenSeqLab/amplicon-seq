4c783689e76bd366c20ab86a49b0c807ecb78afc
jodyphelan <jody.phelan@lshtm.ac.uk>
2021-03-24 14:55:40 +0000

    update to freebayes

--- amplicon_script.py
+++ amplicon_script.py
@@ -23,34 +23,34 @@ def main(args):
     fm.faidx(args.ref)
 
     run_cmd("demultiplex_fastq.py --R1 %(read1)s --R2 %(read2)s --index %(index_file)s" % vars(args))
-
+    
     for sample in samples:
         args.sample = sample
-        run_cmd("trimmomatic PE %(sample)s_1.fastq.gz %(sample)s_2.fastq.gz %(sample)s_1.trimmed.fastq.gz %(sample)s_1.untrimmed.fastq.gz %(sample)s_2.trimmed.fastq.gz %(sample)s_2.untrimmed.fastq.gz LEADING:3 TRAILING:3 SLIDINGWINDOW:4:30 MINLEN:36" % vars(args))
-        run_cmd("bwa mem -t 10 -R \"@RG\\tID:%(sample)s\\tSM:%(sample)s\\tPL:Illumina\" %(ref)s %(sample)s_1.trimmed.fastq.gz %(sample)s_2.trimmed.fastq.gz | samtools sort -o %(sample)s.bam -" % vars(args))
+        run_cmd("fastqc %(sample)s_1.fastq.gz %(sample)s_2.fastq.gz" % vars(args))
+        if args.trim:
+            run_cmd("trimmomatic PE %(sample)s_1.fastq.gz %(sample)s_2.fastq.gz %(sample)s_1.trimmed.fastq.gz %(sample)s_1.untrimmed.fastq.gz %(sample)s_2.trimmed.fastq.gz %(sample)s_2.untrimmed.fastq.gz LEADING:3 TRAILING:3 SLIDINGWINDOW:4:%(trim_qv)s MINLEN:36 2> %(sample)s.trimlog" % vars(args))
+            run_cmd("bwa mem -t 10 -R \"@RG\\tID:%(sample_prefix)s%(sample)s\\tSM:%(sample_prefix)s%(sample)s\\tPL:Illumina\" %(ref)s %(sample)s_1.trimmed.fastq.gz %(sample)s_2.trimmed.fastq.gz | samtools sort -o %(sample)s.bam -" % vars(args))
+        else:
+            run_cmd("bwa mem -t 10 -R \"@RG\\tID:%(sample_prefix)s%(sample)s\\tSM:%(sample_prefix)s%(sample)s\\tPL:Illumina\" %(ref)s %(sample)s_1.fastq.gz %(sample)s_2.fastq.gz | samtools sort -o %(sample)s.bam -" % vars(args))
+
         run_cmd("samtools index %(sample)s.bam" % vars(args))
-        run_cmd("samtools flagstat %(sample)s.bam > %(sample)s_flagstat.txt" % vars(args))
-        run_cmd("bedtools coverage -a %(bed)s -b %(sample)s.bam -mean > %(sample)s_coverage_aedes_mean.txt" % vars(args))
-        run_cmd("samtools stats %(sample)s.bam > %(sample)s.stats" % vars(args)) # needed?
-        run_cmd("gatk HaplotypeCaller -R %(ref)s -L %(bed)s -I %(sample)s.bam -O %(sample)s.gatk.vcf.gz -ERC BP_RESOLUTION -mbq 30" % vars(args)) # gatk -> g?
+        run_cmd("samtools flagstat %(sample)s.bam > %(sample)s.flagstat.txt" % vars(args))
+        run_cmd("mosdepth -x -b %(bed)s %(sample)s --thresholds 1,10,20,30  %(sample)s.bam" % vars(args))
 
-    with open("vcfout.list","w") as O:
+    with open("bam_list.txt","w") as O:
         for s in samples:
-            O.write("%s.gatk.vcf.gz\n" % (s))
+            O.write("%s.bam\n" % (s))
 
-    run_cmd("gatk CombineGVCFs -R %(ref)s -L %(bed)s --variant vcfout.list -O combined.gatk.vcf" % vars(args))
-    run_cmd("gatk GenotypeGVCFs -R %(ref)s -L %(bed)s -V combined.gatk.vcf -O combined_genotyped.gatk.vcf.gz" % vars(args))
+    run_cmd("freebayes -f %(ref)s -t %(bed)s -L bam_list.txt --haplotype-length -1 | bcftools norm -f %(ref)s | bcftools sort -Oz -o combined.genotyped.vcf.gz" % vars(args))
 
-    run_cmd("bcftools filter -i 'FMT/DP>10 & FMT/GQ>30' -S . combined_genotyped.gatk.vcf.gz -Oz -o combined_genotyped.gatk.filtered.vcf.gz")
-    run_cmd("bcftools index combined_genotyped.gatk.filtered.vcf.gz" % vars(args))
-
-    # tranlation
-    run_cmd("bcftools csq -p a -f %(ref)s -g %(gff)s combined_genotyped.gatk.filtered.vcf.gz -Oz -o combined_genotyped.gatk.filtered.trans.vcf.gz" % vars(args))
-    run_cmd("bcftools index combined_genotyped.gatk.filtered.trans.vcf.gz" % vars(args))
-
-    run_cmd("bcftools query combined_genotyped.gatk.filtered.vcf.gz -f '[%SAMPLE\t%POS\t%REF\t%ALT\t%QUAL\t%GQ\t%GT\t%TGT\t%DP\t%AD\n]' > combined_genotyped_filtered_formatted.gatk.txt")
-
-    run_cmd("bcftools query combined_genotyped.gatk.filtered.trans.vcf.gz -f '[%SAMPLE\t%POS\t%REF\t%ALT\t%QUAL\t%GQ\t%GT\t%TGT\t%DP\t%AD\t%INFO/BCSQ\n]' > combined_genotyped_filtered_formatted.trans.gatk.txt")
+    run_cmd("bcftools filter -i 'FMT/DP>10' -S . combined.genotyped.vcf.gz | bcftools view -i 'QUAL>30' | bcftools sort | bcftools norm -m - -Oz -o tmp.vcf.gz" % vars(args))
+    run_cmd("bcftools view -v snps tmp.vcf.gz | bcftools csq -p a -f %(ref)s -g %(gff)s -Oz -o tmp.snps.vcf.gz" % vars(args))
+    run_cmd("tabix tmp.snps.vcf.gz" % vars(args))
+    run_cmd("bcftools view -v indels tmp.vcf.gz | bcftools csq -p a -f %(ref)s -g %(gff)s -Oz -o tmp.indels.vcf.gz" % vars(args))
+    run_cmd("tabix tmp.indels.vcf.gz" % vars(args))
+    run_cmd("bcftools concat -a tmp.snps.vcf.gz tmp.indels.vcf.gz | bcftools sort -Oz -o combined.genotyped.csq.vcf.gz" % vars(args))
+    run_cmd("bcftools index combined.genotyped.csq.vcf.gz" % vars(args))
+    run_cmd(r"bcftools query combined.genotyped.csq.vcf.gz -f '[%SAMPLE\t%CHROM\t%POS\t%REF\t%ALT\t%QUAL\t%GT\t%TGT\t%DP\t%AD\t%INFO/BCSQ\n]' > combined_genotyped_filtered_formatted.trans.gatk.txt")
 
 
 
@@ -62,6 +62,9 @@ parser.add_argument('--read2',type=str,help='File suffix',required=True)
 parser.add_argument('--ref',type=str,help='File suffix',required=True)
 parser.add_argument('--gff',type=str,help='File suffix',required=True)
 parser.add_argument('--bed',type=str,help='File suffix',required=True)
+parser.add_argument('--trim',action="store_true",help='File suffix')
+parser.add_argument('--sample-prefix',default="",help='File suffix')
+parser.add_argument('--trim-qv',default=20,type=int,help='File suffix')
 parser.set_defaults(func=main)
 
 args = parser.parse_args()
---
+++ demultiplex_fastq.py
@@ -0,0 +1,77 @@
+#! /usr/bin/env python
+import sys
+import argparse
+import csv
+import subprocess
+from tqdm import tqdm
+import gzip
+
+def num_mismatches(str_x,str_y):
+    mismatches = 0
+    for char_x,char_y in zip(str_x,str_y):
+        if char_x != char_y:
+            mismatches+=1
+    return mismatches
+
+def main(args):
+    index = {}
+    for row in csv.DictReader(open(args.index)):
+        if row["sample"]=="": continue
+        index[(row["I1"],row["I2"])] = row["sample"]
+        index_size = len(row["I1"])
+    R1 = gzip.open(args.R1)
+    R2 = gzip.open(args.R2)
+    R1_FILES = {s:gzip.open("%s_1.fastq.gz" % s,"wb") for s in index.values()}
+    R2_FILES = {s:gzip.open("%s_2.fastq.gz" % s,"wb") for s in index.values()}
+    for l in subprocess.Popen("gunzip -c %s | wc -l" % args.R1, shell=True,stdout=subprocess.PIPE).stdout:
+        num_lines = int(l.strip())
+    reads_assigned = {s:0 for s in index.values()}
+    reads_assigned["not_assigned"] = 0
+    for i in tqdm(range(int(num_lines/4))):
+    # for i in range(4):
+        data1 = [R1.readline() for _ in range(4)]
+        data2 = [R2.readline() for _ in range(4)]
+        # print(data1[0].decode())
+        # print(data1)
+
+        indices = (data1[1].decode()[:index_size],data2[1].decode()[:index_size])
+        if indices in index:
+            sample = index[indices]
+            reads_assigned[sample]+=1
+            for i in range(4):
+                R1_FILES[sample].write(data1[i])
+                R2_FILES[sample].write(data2[i])
+
+        else:
+            mismatches = {}
+            possible_matches = {}
+            for i1,i2 in index:
+                mismatch_r1 = num_mismatches(i1,indices[0])
+                mismatch_r2 = num_mismatches(i2,indices[1])
+                mismatches[index[i1,i2]] = (mismatch_r1,mismatch_r2)
+                if mismatch_r1<=args.max_mismatches and mismatch_r2<=args.max_mismatches:
+                    possible_matches[index[i1,i2]] = (i1,i2)
+            if len(list(possible_matches.keys()))==1:
+                sample = list(possible_matches.keys())[0]
+                reads_assigned[sample]+=1
+                for i in range(4):
+                    R1_FILES[sample].write(data1[i])
+                    R2_FILES[sample].write(data2[i])
+                # R2_FILES[sample].write("".join(data2))
+            else:
+                reads_assigned["not_assigned"]+=1
+
+
+    print(reads_assigned)
+
+
+
+parser = argparse.ArgumentParser(description='TBProfiler pipeline',formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('--R1','-1',type=str,help='NGS Platform',required=True)
+parser.add_argument('--R2','-2',type=str,help='NGS Platform',required=True)
+parser.add_argument('--index',type=str,help='NGS Platform',required=True)
+parser.add_argument('--max-mismatches',default=1,type=int,help='NGS Platform')
+parser.set_defaults(func=main)
+
+args = parser.parse_args()
+args.func(args)
