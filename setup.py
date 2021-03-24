79619de3ffc6664e86da56982f35727c9756207f
jodyphelan <jody.phelan@lshtm.ac.uk>
2021-03-24 15:01:04 +0000

    update to package

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+import os
+
+
+setuptools.setup(
+
+	name="amplicon-seq",
+
+	version="0.0.1",
+	packages=["amplicon_seq"],
+	license="MIT",
+	long_description="Amplicon sequencing command line tool",
+	scripts= ["scripts/%s" % x for x in os.listdir("scripts")],
+)
