c1f306edaa4ebd06924e656255c5b67a87c9c9ff
jodyphelan <jody.phelan@lshtm.ac.uk>
2021-06-04 14:29:36 +0100

    add simple variant calling script

--- scripts/amplicon_variant_calling.py
+++ scripts/amplicon_variant_calling.py
@@ -15,11 +15,11 @@ def main(args):
     fm.bwa_index(args.ref)
     fm.faidx(args.ref)
 
-    fm.run_cmd("demultiplex_fastq.py --R1 %(read1)s --R2 %(read2)s --index %(index_file)s" % vars(args))
+    # fm.run_cmd("demultiplex_fastq.py --R1 %(read1)s --R2 %(read2)s --index %(index_file)s" % vars(args))
 
     for sample in samples:
         args.sample = sample
-        fm.run_cmd("bwa mem -t 10 -R \"@RG\\tID:%(sample_prefix)s%(sample)s\\tSM:%(sample_prefix)s%(sample)s\\tPL:Illumina\" %(ref)s %(sample)s_1.fastq.gz %(sample)s_2.fastq.gz | samclip --ref %(ref)s --max 50 | samtools sort -o %(sample)s.bam -" % vars(args))
+        fm.run_cmd("bwa mem -t 10 -R \"@RG\\tID:%(sample)s\\tSM:%(sample)s\\tPL:Illumina\" %(ref)s %(sample)s_1.fastq.gz %(sample)s_2.fastq.gz | samclip --ref %(ref)s --max 50 | samtools sort -o %(sample)s.bam -" % vars(args))
 
         fm.run_cmd("samtools index %(sample)s.bam" % vars(args))
         fm.run_cmd("freebayes -f %(ref)s --haplotype-length -1 %(sample)s.bam --min-base-quality %(min_base_qual)s | bcftools view -Oz -o %(sample)s.vcf.gz" % vars(args))
@@ -29,6 +29,7 @@ parser.add_argument('--index-file',type=str,help='CSV file containing fields "Sa
 parser.add_argument('--read1',type=str,help='Forward read file',required=True)
 parser.add_argument('--read2',type=str,help='Reverse read file',required=True)
 parser.add_argument('--ref',type=str,help='Reference fasta',required=True)
+parser.add_argument('--min-base-qual',default=30,type=int,help='Minimum base quality to use by freebayes')
 parser.set_defaults(func=main)
 args = parser.parse_args()
 args.func(args)
