962b04cf65a145c094da57cd6081f879aa0e6f7c
jodyphelan <jody.phelan@lshtm.ac.uk>
2021-11-01 17:01:37 +0000

    changes

--- scripts/amplicon_combine_samples.py
+++ scripts/amplicon_combine_samples.py
@@ -1,12 +1,12 @@
+from collections import defaultdict
 import sys
 import argparse
 import subprocess as sp
 import csv
 import fastq2matrix as fm
 from fastq2matrix import run_cmd
-
-
-
+from tqdm import tqdm
+import gzip
 
 
 def main(args):
@@ -17,48 +17,72 @@ def main(args):
                 sys.stderr.write(f"Warning! You have a duplicate sample name: {row['sample']}\n")
             samples.append(row["sample"])
 
-    with open("vcf_files.txt","w") as O:
-        for s in samples:
-            O.write(f"{s}.freebayes.vcf\n")
-            O.write(f"{s}.gatk.vcf\n")
-    for sample in samples:
-        args.sample = sample
-        run_cmd("naive_variant_caller.py --ref %(ref)s --bam %(sample)s.bam --sample %(sample)s --min-af %(min_sample_af)s --vcf-file-list vcf_files.txt | bcftools view -Oz -o %(sample)s.vcf.gz" % vars(args))
-        run_cmd("tabix -f %(sample)s.vcf.gz" % vars(args))
-    with open("vcf_list.txt","w") as O:
-        for s in samples:
-            O.write("%s.vcf.gz\n" % (s))
-    run_cmd("bcftools merge -l vcf_list.txt -Oz -o combined.vcf.gz" )
-    run_cmd(r"bcftools query -f '%CHROM\t%POS[\t%DP]\n' combined.vcf.gz > tmp.txt")
+    # with open("vcf_files.txt","w") as O:
+    #     for s in samples:
+    #         O.write(f"{s}.freebayes.vcf\n")
+    #         O.write(f"{s}.gatk.vcf\n")
+    # for sample in samples:
+    #     args.sample = sample
+    #     run_cmd("naive_variant_caller.py --ref %(ref)s --bam %(sample)s.bam --sample %(sample)s --min-af %(min_sample_af)s --vcf-file-list vcf_files.txt | bcftools view -Oz -o %(sample)s.vcf.gz" % vars(args))
+    #     run_cmd("tabix -f %(sample)s.vcf.gz" % vars(args))
+    # with open("vcf_list.txt","w") as O:
+    #     for s in samples:
+    #         O.write("%s.vcf.gz\n" % (s))
+    # run_cmd("bcftools merge -l vcf_list.txt -Oz -o combined.vcf.gz" )
 
-    run_cmd("bcftools filter -i 'FMT/DP>10' -S . combined.vcf.gz | bcftools sort -Oz -o tmp.vcf.gz" % vars(args))
-    run_cmd("bcftools view -v snps tmp.vcf.gz | bcftools csq -p a -f %(ref)s -g %(gff)s -Oz -o snps.vcf.gz" % vars(args))
-    run_cmd("tabix snps.vcf.gz" % vars(args))
-    run_cmd("bcftools view -v indels tmp.vcf.gz | bcftools csq -p a -f %(ref)s -g %(gff)s -Oz -o indels.vcf.gz" % vars(args))
-    run_cmd("tabix indels.vcf.gz" % vars(args))
-    run_cmd(r"bcftools query snps.vcf.gz -f '[%SAMPLE\t%CHROM\t%POS\t%REF\t%ALT\t%QUAL\t%GT\t%TGT\t%DP\t%AD\n]' > combined_genotyped_filtered_formatted.snps.txt")    
-    run_cmd(r"bcftools query snps.vcf.gz -f '[%SAMPLE\t%CHROM\t%POS\t%REF\t%ALT\t%QUAL\t%GT\t%TGT\t%DP\t%AD\t%TBCSQ\n]' > combined_genotyped_filtered_formatted.snps.trans.txt")
-    run_cmd(r"bcftools query indels.vcf.gz -f '[%SAMPLE\t%CHROM\t%POS\t%REF\t%ALT\t%QUAL\t%GT\t%TGT\t%DP\t%AD\n]' > combined_genotyped_filtered_formatted.indels.txt")
-    run_cmd(r"bcftools query indels.vcf.gz -f '[%SAMPLE\t%CHROM\t%POS\t%REF\t%ALT\t%QUAL\t%GT\t%TGT\t%DP\t%AD\t%TBCSQ\n]' > combined_genotyped_filtered_formatted.indels.trans.txt")
+    # run_cmd("bcftools filter -i 'FMT/DP>10' -S . combined.vcf.gz | bcftools sort -Oz -o tmp.vcf.gz" % vars(args))
+    # run_cmd("bcftools view -v snps tmp.vcf.gz | bcftools csq -p a -f %(ref)s -g %(gff)s -Oz -o snps.vcf.gz" % vars(args))
+    # run_cmd("tabix snps.vcf.gz" % vars(args))
+    # run_cmd("bcftools view -v indels tmp.vcf.gz | bcftools csq -p a -f %(ref)s -g %(gff)s -Oz -o indels.vcf.gz" % vars(args))
+    # run_cmd("tabix indels.vcf.gz" % vars(args))
+    # run_cmd(r"bcftools query snps.vcf.gz -f '[%SAMPLE\t%CHROM\t%POS\t%REF\t%ALT\t%QUAL\t%GT\t%TGT\t%DP\t%AD\n]' > combined_genotyped_filtered_formatted.snps.txt")    
+    # run_cmd(r"bcftools query snps.vcf.gz -f '[%SAMPLE\t%CHROM\t%POS\t%REF\t%ALT\t%QUAL\t%GT\t%TGT\t%DP\t%AD\t%TBCSQ\n]' > combined_genotyped_filtered_formatted.snps.trans.txt")
+    # run_cmd(r"bcftools query indels.vcf.gz -f '[%SAMPLE\t%CHROM\t%POS\t%REF\t%ALT\t%QUAL\t%GT\t%TGT\t%DP\t%AD\n]' > combined_genotyped_filtered_formatted.indels.txt")
+    # run_cmd(r"bcftools query indels.vcf.gz -f '[%SAMPLE\t%CHROM\t%POS\t%REF\t%ALT\t%QUAL\t%GT\t%TGT\t%DP\t%AD\t%TBCSQ\n]' > combined_genotyped_filtered_formatted.indels.trans.txt")
 
+    bedlines = []
+    amplicon_positions = []
+    for l in open("/home/jody/projects/emma/Reference_files/amplicon_pos_ref.bed"):
+        row = l.strip().split()
+        bedlines.append(row)        
+        for p in range(int(row[1]),int(row[2])):
+            amplicon_positions.append((row[0],p))
 
-    
+    def overlap_bedlines(a,bedlines):
+        overlaps = []
+        for b in bedlines:
+            if b[0]==a[0]:
+                overlap = max(0, min(int(a[2]), int(b[2])) - max(int(a[1]), int(b[1])))
+                if overlap>0:
+                    overlaps.append([b[0],max(int(a[1]),int(b[1])),min(int(a[2]),int(b[2]))])
+        return overlaps
+
+    dp = defaultdict(dict)
+    for s in tqdm(samples):
+        for l in gzip.open(f"{s}.per-base.bed.gz"):
+            row = l.decode().strip().split()
+            overlaps = overlap_bedlines(row,bedlines)
+            if len(overlaps)>0:
+                for overlap in overlaps:
+                    for pos in range(int(overlap[1]),int(overlap[2])):
+                        dp[s][(row[0],pos)] = int(row[3])
+
     pos_info = {}
     for l in open(args.position_info):
         row = l.strip().split()
-        pos_info[(row[0],row[1])] = (row[2],row[3])
+        pos_info[(row[0],int(row[1]))] = (row[2],row[3])
+    
     with open("depth_info.txt", "w") as O:
         O.write("chrom\tpos\tgene\tcsq\t%s\n" % "\t".join(samples))
-        for l in open("tmp.txt"):
-            row = l.strip().split()
-            if (row[0],row[1]) in pos_info:
-                p = pos_info[(row[0],row[1])]
-                row.insert(2,p[1])
-                row.insert(2,p[0])
-            else:
-                row.insert(2,"NA")
-                row.insert(2,"NA")
-            O.write("%s\n" % ("\t".join(row)))
+        for chrom,pos in amplicon_positions:
+            if (chrom,pos) in pos_info:
+                d = pos_info[(chrom,pos)]
+                O.write("%s\t%s\t%s\t%s\t%s\n" % (
+                    chrom,pos,d[0],d[1],
+                    "\t".join([str(dp[s].get((chrom,pos),0)) for s in samples])
+                ))
+
+        
 
 
 
